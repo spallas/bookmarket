b5727b0e451f5d730a2917be82fb093e671bc4a3
Leonardo <leonardosagratella@gmail.com>
2019-01-29 17:44:39 +0100

    Added firebase login, google still not working

--- app/src/main/java/com/gnufsociety/bookmarket/MainActivity.java
+++ app/src/main/java/com/gnufsociety/bookmarket/MainActivity.java
@@ -1,13 +1,63 @@
 package com.gnufsociety.bookmarket;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        createSignInIntent();
+
+    }
+
+    public void createSignInIntent() {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setLogo(R.drawable.ic_launcher_foreground)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN){
+            if (resultCode == RESULT_OK){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                setContentView(R.layout.activity_main);
+                TextView username = findViewById(R.id.user_name);
+                username.setText(user.getEmail());
+
+                // TODO check if the user exists on rails backend, if yes jump to home activity, otherwise remain on complete profile
+
+            } else if (resultCode == RESULT_CANCELED) {
+                createSignInIntent();
+            } else {
+                // HANDLE ERRORS HERE???? che ce frega a noi!
+            }
+        }
     }
 }
