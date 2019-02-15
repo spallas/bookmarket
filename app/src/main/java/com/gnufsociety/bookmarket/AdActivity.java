4710a669abc1fb2864dc6b6078f1618f37af943a
spallas <davide.spallaccini@gmail.com>
2019-02-16 00:45:47 +0100

    UI fixes.

--- app/src/main/java/com/gnufsociety/bookmarket/AdActivity.java
+++ app/src/main/java/com/gnufsociety/bookmarket/AdActivity.java
@@ -38,7 +38,7 @@ public class AdActivity extends AppCompatActivity {
 
         adTitle.setText(ad.getBook().getTitle());
         adAuthor.setText(ad.getBook().getAuthor());
-        adPrice.setText("Price: €" + ad.getPrice());
+        adPrice.setText("€" + ad.getPrice());
         adDesc.setText(ad.getDescription());
         adSeller.setText(ad.getUser().getUsername());
 
